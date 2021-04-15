8cf736472708880f24c34cd854fba35a93599b8b
ankit-tailor <tailorankit3345@gmail.com>
2021-04-15 15:38:07 +0530

    add feeds

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,14 +1,40 @@
 import React from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const Title = ({ clickHandel, button, description }) => {
+const Title = ({
+  setAuthenticate,
+  description,
+  setClick,
+  click,
+  setShowError,
+}) => {
+  const [user] = useAuthState(projectAuth);
+
   return (
     <div className="title">
       <div className="header">
-        <h1>FireGram</h1>
-        <button onClick={clickHandel}>{button}</button>
+        <h1>PhotoGram</h1>
+        <div className="header__personal">
+          <button
+            onClick={() => {
+              setClick(!click);
+              !user && setShowError(true);
+            }}
+          >
+            {!click ? "Your Posts" : "Public"}
+          </button>
+          <button
+            onClick={() => {
+              user ? projectAuth.signOut() : setAuthenticate(true);
+            }}
+          >
+            {user ? "Sign Out" : "Sign In"}
+          </button>
+        </div>
       </div>
       <hr />
-      <h2>Your Pictures</h2>
+      <h2>{click ? "Your Feed" : "Public Feed"}</h2>
       <p>{description}</p>
     </div>
   );
