db32bb89500a3289ff15998cfd5960c10ea5e535
ankit-tailor <tailorankit3345@gmail.com>
2021-04-16 18:50:03 +0530

    add feature visibility to make post public/private

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ImageGrid.css";
 import useFirestore from "../hooks/useFirestore";
 import { motion } from "framer-motion";
@@ -11,9 +11,10 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import EditModal from "./EditModal";
-import { useState } from "react";
 import ErrorModal from "./ErrorModal";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 
 const ImageGrid = ({ setImage, click, showError, setShowError, setClick }) => {
   const { imageData } = useFirestore("image");
@@ -64,6 +65,12 @@ const ImageGrid = ({ setImage, click, showError, setShowError, setClick }) => {
     }
   };
 
+  const handelVisibility = (id, visibility) => {
+    projectFirestore.collection("image").doc(id).update({
+      visibility: !visibility,
+    });
+  };
+
   const handelEdit = (id) => {
     setClicked(!clicked);
     setImageId(id);
@@ -72,55 +79,64 @@ const ImageGrid = ({ setImage, click, showError, setShowError, setClick }) => {
   return (
     <div className={click ? "grid" : "img-grid"}>
       {imageData && !click
-        ? imageData.map((image) => (
-            <motion.div
-              key={image.id}
-              className="post-section"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            >
-              <motion.div
-                className="img-wrap"
-                onClick={() => {
-                  setImage(image.url);
-                }}
-                whileHover={{ opacity: 1 }}
-                layout
-              >
-                <motion.img
-                  loading="lazy"
-                  src={image.url}
-                  alt="uploaded_image"
+        ? imageData.map(
+            (image) =>
+              image.visibility && (
+                <motion.div
+                  key={image.id}
+                  className="post-section"
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ delay: 1 }}
-                />
-              </motion.div>
-              <p>{image.caption}</p>
-              <p>{image.likeCount} likes</p>
-              <div className="update-section">
-                <FavoriteBorderIcon
-                  className="favourite-icon"
-                  onClick={() => {
-                    handelLike(image.id, image.likeCount);
-                  }}
-                />
-                {user && projectAuth.currentUser.uid === image.userId && (
-                  <div>
-                    <EditIcon
-                      className="edit-icon"
-                      onClick={() => handelEdit(image.id)}
+                >
+                  <motion.div
+                    className="img-wrap"
+                    onClick={() => {
+                      setImage(image.url);
+                    }}
+                    whileHover={{ opacity: 1 }}
+                    layout
+                  >
+                    <motion.img
+                      loading="lazy"
+                      src={image.url}
+                      alt="uploaded_image"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ delay: 1 }}
                     />
-                    <DeleteIcon
-                      className="delete-icon"
-                      onClick={() => handelDelete(image.id, image.fileName)}
+                  </motion.div>
+                  <p>{image.caption}</p>
+                  <p>{image.likeCount} likes</p>
+                  <div className="update-section">
+                    <FavoriteBorderIcon
+                      className="favourite-icon"
+                      onClick={() => {
+                        handelLike(image.id, image.likeCount);
+                      }}
                     />
+                    {user && projectAuth.currentUser.uid === image.userId && (
+                      <div>
+                        <VisibilityIcon
+                          className="visibility-icon"
+                          onClick={() =>
+                            handelVisibility(image.id, image.visibility)
+                          }
+                        />
+                        <EditIcon
+                          className="edit-icon"
+                          onClick={() => handelEdit(image.id)}
+                        />
+                        <DeleteIcon
+                          className="delete-icon"
+                          onClick={() => handelDelete(image.id, image.fileName)}
+                        />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            </motion.div>
-          ))
+                </motion.div>
+              )
+          )
         : !user
         ? showError && (
             <ErrorModal
@@ -168,6 +184,21 @@ const ImageGrid = ({ setImage, click, showError, setShowError, setClick }) => {
                     />
                     {user && projectAuth.currentUser.uid === image.userId && (
                       <div>
+                        {image.visibility ? (
+                          <VisibilityIcon
+                            className="visibility-icon"
+                            onClick={() =>
+                              handelVisibility(image.id, image.visibility)
+                            }
+                          />
+                        ) : (
+                          <VisibilityOffIcon
+                            className="visibility-icon"
+                            onClick={() =>
+                              handelVisibility(image.id, image.visibility)
+                            }
+                          />
+                        )}
                         <EditIcon
                           className="edit-icon"
                           onClick={() => handelEdit(image.id)}
