eeb3ff65cb4b6ec4283071a94683d5c2bc9ad09d
ankit-tailor <tailorankit3345@gmail.com>
2021-04-16 11:21:29 +0530

    import css

--- src/comps/ErrorModal.js
+++ src/comps/ErrorModal.js
@@ -1,20 +1,34 @@
+import { motion } from "framer-motion";
 import React from "react";
+import "./ErrorModal.css";
 
 const ErrorModal = ({ title, setShowError, setClick }) => {
   return (
-    <div className="backdrop">
-      <div className="error-message">
-        <h2>{title}</h2>
-        <button
+    <motion.div
+      className="backdrop"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <div
+        className="error-message"
+        inital={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+      >
+        <motion.h2 inital={{ y: "-100vh" }} animate={{ y: 0 }}>
+          {title}
+        </motion.h2>
+        <motion.button
+          inital={{ y: "-100vh" }}
+          animate={{ y: 0 }}
           onClick={() => {
             setShowError(false);
             setClick(false);
           }}
         >
           Dismiss
-        </button>
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
